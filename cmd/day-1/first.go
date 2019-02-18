1efc079794d2017f17b4888b43e33bf5e623dca1
Zack Stayman <zack@Zacks-MBP.fios-router.home>
2019-02-18 18:35:39 -0500

    both halves of day 1 completed

--- cmd/day-1/first.go
+++ cmd/day-1/first.go
@@ -26,6 +26,33 @@ func main() {
 		res += intVal
 	}
 
-	fmt.Println(res)
+	fmt.Printf("The result of the sequence is: %d\n", res)
 
+	values := make(map[int]int)
+	var res2 int
+	var repeat int
+	var done bool
+
+	for {
+		for _, val := range vals {
+			intVal, err := strconv.Atoi(val)
+			if err != nil {
+				panic(fmt.Sprintf("Bad Input: %v", err))
+			}
+			res2 += intVal
+			values[res2] = values[res2] + 1
+
+			if values[res2] > 1 {
+				repeat = res2
+				done = true
+				break
+			}
+		}
+
+		if done {
+			break
+		}
+	}
+
+	fmt.Printf("The first duplicated frequency is: %d\n", repeat)
 }
